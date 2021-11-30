b4586f237f0d3eea201e384753158a3f89a9d5d7
SagyngaliMamay <sagingali.mamayev@gmail.com>
2021-11-30 18:17:22 +0100

    creation models by creation classes

--- e_shop/mainapp/models.py
+++ e_shop/mainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+#1 Category:
+#2 Product
+#3 CartProduct
+#4 Cart
+#5 Order
+#6 Customer
+#7 Description/Specification
+
+# Here I create my first model by class
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Name of Category')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+# Second model
+class Product(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Name of product')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField()
+    description = models.TextField(verbose_name='Description', null=True)#<-- null = True means that this filed can be empty
+    price = models.TextField(max_digits=9, decimal_places=2, verbose_name='price')
+
